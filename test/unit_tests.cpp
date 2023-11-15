329d531f688be6aeaf0f6871a1e38f691141301c
Armin Sobhani <asobhani@sharcnet.ca>
2023-11-15 15:45:24 -0500

    fix problem with string literal

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,5 +1,6 @@
-#include <catch2/catch_all.hpp>
+#include <cstring>
 
+#include <catch2/catch_all.hpp>
 #include <sq3p/sq.hpp>
 
 TEMPLATE_TEST_CASE( "sq3p::sq", "[class]", std::vector<char>)
@@ -14,14 +15,14 @@ TEMPLATE_TEST_CASE( "sq3p::sq", "[class]", std::vector<char>)
     s["int"] = 19;
     CHECK(19 == std::any_cast<int>(s["int"]));
 
-    // s["float"] = 3.14f;
-    // CHECK(3.14f == std::any_cast<float>(s["float"]));
+    s["float"] = 3.14f;
+    CHECK(3.14f == std::any_cast<float>(s["float"]));
 
-    // s["double"] = 3.14;
-    // CHECK(3.14 == std::any_cast<double>(s["double"]));
+    s["double"] = 3.14;
+    CHECK(3.14 == std::any_cast<double>(s["double"]));
 
     s["string_literal"] = "hello";
-    CHECK("hello" == std::any_cast<const char*>(s["string_literal"]));
+    CHECK(0 == std::strcmp("hello", std::any_cast<const char*>(s["string_literal"])));
 
     s["string"] = std::string("hello");
     CHECK("hello" == std::any_cast<std::string>(s["string"]));
